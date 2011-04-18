8a4961b09b00c2bebf0fa1cbaebb36f481a71208
Julien Gribonvald <julien.gribonvald@gmail.com>
2011-04-18 15:17:52 +0000

    Correction de bug : le nom de la constante définie pour le nom du paramètre du filterId n'était pas correct, faisant que la suppression et l'édition d'un filtre ne fonctionnait plus (bug introduit lors de la dernière version pour la compatibilité uPortail 3.2 etplus particulièrement lors d'un "nettoyage" sur les noms des variables des formulaires.

--- src/org/uhp/portlets/news/web/support/Constants.java
+++ src/org/uhp/portlets/news/web/support/Constants.java
@@ -1,16 +1,16 @@
 /**
- * @Project NewsPortlet : http://sourcesup.cru.fr/newsportlet/ 
+ * @Project NewsPortlet : http://sourcesup.cru.fr/newsportlet/
  * Copyright (C) 2007-2008 University Nancy 1
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
@@ -121,7 +121,7 @@ public final class Constants {
     public static final String ATT_TYPE_ID = "typId";
     public static final String ATT_CTX_ID = "ctxId";
     public static final String ATT_ENTITY_ID = "eId";
-    public static final String ATT_FILTER_ID = "typId";
+    public static final String ATT_FILTER_ID = "fId";
 
     public static final String OBJ_USER = "user";
     public static final String OBJ_ITEM = "item";
